9286841c69121eb79c37f0d30af24325879cb12a
natsu1dragneel <40191775+natsu1dragneel@users.noreply.github.com>
2019-07-27 01:49:56 +0530

    Add files via upload

---
+++ twitterbot.py
@@ -0,0 +1,19 @@
+import tweepy as tp
+import time
+import os
+
+#login into twitter api                               #provide it yourself
+consumer_key=''                                                      
+consumer_secret=''
+access_token=''
+access_secret=''
+
+auth=tp.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_secret)
+api=tp.API(auth)
+
+#iterates the folder content
+os.chdir('')
+for folder_name in os.listdir('.'):
+    api.update_with_media(folder_name)                          #api.update_status be used for text based content
+    time.sleep(3600)
